2619a11cc23c63e943d7fb9c733fe7b5c1dff3b2
yashv625 <yverma913@gmail.com>
2014-11-24 13:25:12 +0530

    Create client.java

---
+++ client.java
@@ -0,0 +1,74 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.awt.Desktop;
+import java.io.File;
+
+public class client {
+
+  public final static int SOCKET_PORT = 9119;      // you may change this
+  public final static String SERVER = "127.0.0.1";  // localhost
+  public final static String
+       FILE_TO_RECEIVED = "/Users/Yash/Desktop/d1.pdf";  // you may change this, I give a
+                                                            // different name because i don't want to
+                                                            // overwrite the one used by server...
+
+  public final static int FILE_SIZE = 6022386; // file size temporary hard coded
+                                               // should bigger than the file to be downloaded
+
+  public static void main (String [] args ) throws IOException {
+    int bytesRead;
+    int current = 0;
+    FileOutputStream fos = null;
+    BufferedOutputStream bos = null;
+    Socket sock = null;
+    try {
+      sock = new Socket(SERVER, SOCKET_PORT);
+      System.out.println("Connecting...");
+
+      // receive file
+      byte [] mybytearray  = new byte [FILE_SIZE];
+      InputStream is = sock.getInputStream();
+      fos = new FileOutputStream(FILE_TO_RECEIVED);
+      bos = new BufferedOutputStream(fos);
+      bytesRead = is.read(mybytearray,0,mybytearray.length);
+      current = bytesRead;
+
+      do {
+         bytesRead =
+            is.read(mybytearray, current, (mybytearray.length-current));
+         if(bytesRead >= 0) current += bytesRead;
+      } while(bytesRead > -1);
+
+      bos.write(mybytearray, 0 , current);
+      bos.flush();
+      System.out.println("File " + FILE_TO_RECEIVED
+          + " downloaded (" + current + " bytes read)");
+
+      //to open file
+      File pdfFile = new File("/Users/Yash/Desktop/d1.pdf");
+		if (pdfFile.exists()) {
+
+			if (Desktop.isDesktopSupported()) {
+				Desktop.getDesktop().open(pdfFile);
+			} else {
+				System.out.println("Awt Desktop is not supported!");
+			}
+
+		} else {
+			System.out.println("File is not exists!");
+		}
+
+		System.out.println("Done");
+
+    }
+    finally {
+      if (fos != null) fos.close();
+      if (bos != null) bos.close();
+      if (sock != null) sock.close();
+    }
+  }
+
+}
